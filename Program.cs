91414317a5d161ee9bb0414b71eab75a13dc9547
Bryan Pritchett <dasassygoat@coderbox.co>
2024-07-15 19:01:35 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+
+Console.WriteLine("Hello, World!");
+
+string testText = "This is some new text";
+
+Console.WriteLine(testText);
+
+var teeHeeHee = "body count";
+
+string numb = "4";
+
+string numbNuttz = "some string";
+
+Console.WriteLine("First: " + numb + " Second: " + numbNuttz + " And original: " + teeHeeHee);
+
+var temp = new List<string>();
+temp.Add("item1");
